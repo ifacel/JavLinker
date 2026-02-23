79ffff4b0d218ea988dc101eac6637c58098649b
ifacel <ifacel@outlook.com>
2026-02-23 19:34:38 +0800

    fix: popup 设置错谁

--- src/popup/setting_page.js
+++ src/popup/setting_page.js
@@ -4,30 +4,31 @@ window.onload = () => {
     const checkboxUseNewTab = document.getElementById('useNewTab')
     const checkboxNewTabInBackend = document.getElementById('newTabInBackend')
 
-    checkboxUseNewTab.addEventListener('change', async (event) => {        
-        let result = await Setting.setUseNewTab(event.target.checked)   
+    checkboxUseNewTab.addEventListener('change', async (event) => {
+        let result = await Setting.setUseNewTab(event.target.checked)
         if (result instanceof Ok) {
-            //后台打开配置依赖于
+            //后台打开依赖于此
             checkboxNewTabInBackend.disabled = !event.target.checked
-        }
-        if (result instanceof Error) {
+        } else if (result instanceof Error) {
+            loadConfig()
             alert(result.message)
         }
     })
 
-    checkboxNewTabInBackend.addEventListener('change', async (event) => {        
+    checkboxNewTabInBackend.addEventListener('change', async (event) => {
         let result = await Setting.setOpenInBackend(event.target.checked)
         if (result instanceof Error) {
+            loadConfig()
             alert(result.message)
         }
     })
 
     async function loadConfig() {
-        let resultUseNewTab = await Setting.getUseNewTab()        
+        let resultUseNewTab = await Setting.getUseNewTab()
         if (resultUseNewTab instanceof Ok) {
             checkboxUseNewTab.checked = resultUseNewTab.data
         }
-        checkboxUseNewTab.disabled = false
+
         let resultOpenInBackend = await Setting.getOpenInBackend()
         if (resultOpenInBackend instanceof Ok) {
             checkboxNewTabInBackend.checked = resultOpenInBackend.data
